ede9523641ec6ff34069e989486d737d52a1a2ae
achen1105 <achen1105@gmail.com>
2022-04-29 23:46:39 -0700

    showing up now

--- 20121225hw4/flutter_application_1/lib/main.dart
+++ 20121225hw4/flutter_application_1/lib/main.dart
@@ -1,8 +1,9 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
+import 'dart:io';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+//import 'globals.dart' as globals;
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,201 +11,259 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    // TITLE SECTION
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
-        ],
-      ),
-    );
-
-    // BUTTON SECTION
-    Color color = Theme.of(context).primaryColor;
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
-    );
-
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
-
-    // PHOTO GRID
-    //Widget photoSection = _buildPhotoColumn();
-
-    /**  
-    Widget photoSection = GridView.count(
-      crossAxisCount: 2,
-      children: <Widget>[
-        FlutterLogo(),
-        FlutterLogo(),
-        FlutterLogo(),
-        FlutterLogo(),
-      ],
-    );
-    */
-    // MENU OPTIONS
-    Widget menuSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        ElevatedButton(
-          //style: style,
-          onPressed: null,
-          child: const Text('Add from photo'),
-        ),
-        const SizedBox(height: 30),
-        ElevatedButton(
-          //style: style,
-          onPressed: () {},
-          child: const Text('Take photo'),
-        ),
-      ],
-    );
-  
-
     return MaterialApp(
-      title: 'CSCI 571 Photo App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Album 571'),
-        ),
-        body: _buildPhotoColumn(),
-        bottomNavigationBar: BottomAppBar(
-          shape: const CircularNotchedRectangle(),
-          //child: Container(height: 50.0),
-          child: menuSection,
-          color: Colors.blue,
-        ),
+      title: 'Album 571',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-
-  // PRIVATE HELPER METHODS
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  // private gridview
-  GridView _buildPhotoColumn() {
-    return GridView.count(
-      primary: false,
-      padding: const EdgeInsets.all(20),
-      crossAxisSpacing: 10,
-      mainAxisSpacing: 10,
-      crossAxisCount: 3,
-      children: <Widget>[
-        Container(
-          padding: const EdgeInsets.all(8),
-          child: const Text("He'd have you all unravel at the"),
-          color: Colors.teal[100],
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          child: const Text('Heed not the rabble'),
-          color: Colors.teal[200],
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          child: const Text('Sound of screams but the'),
-          color: Colors.teal[300],
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          child: const Text('Who scream'),
-          color: Colors.teal[400],
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          child: const Text('Revolution is coming...'),
-          color: Colors.teal[500],
-        ),
-        Container(
-          padding: const EdgeInsets.all(8),
-          child: const Text('Revolution, they...'),
-          color: Colors.teal[600],
-        ),
-      ],
+      home: HomePageState(),
     );
   }
 }
 
-// Notes
-// https://api.flutter.dev/flutter/widgets/GridView-class.html
-/**ListView(
+enum ImageSourceType { gallery, camera }
+
+var photoList = [];
+
+class HomePageState extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePage();
+  }
+}
+
+class HomePage extends State<HomePageState> with WidgetsBindingObserver {
+  // Tap to enter picking image view
+  void getPhoto(BuildContext context, var type) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ImageFromGalleryEx(type)),
+    ).then((value) => setState(() {}));
+  }
+
+  // Tap to enter full screen image view
+  void fullScreenImage(BuildContext context, var path) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => FullScreenState(path)));
+  }
+
+  // https://stackoverflow.com/questions/50441168/iterating-through-a-list-to-render-multiple-widgets-in-flutter
+  @override
+  Widget build(BuildContext context) {
+    //Grid view
+    Widget gridSection = GridView.count(
+      crossAxisCount: 3,
+      children: [
+        // iterate through all the image paths saved in photoList
+        // for each path - set up fullScreenImage display, and a 'child' ClipRRect widget that
+        // will hold as ITS child, an Image (given its path)
+        for (var i in photoList)
+          GestureDetector(
+            onTap: () {
+               fullScreenImage(context, i);
+            },
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.file(
+                  File(i),
+                  // width: 800,
+                  // height: 800,
+                  fit: BoxFit.cover,
+                  ),
+              )
+          ),
+      ]
+    );
+  
+    // Our two buttons - one for gallery imgs, one for camera...
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        MaterialButton(
+          color: Colors.blue,
+          child: const Text(
+            "Add from Gallery",
+            style:
+                TextStyle(color: Colors.white70, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () {
+            getPhoto(context, ImageSourceType.gallery);
+          },
+        ),
+
+        // ADD the button for camera capture, similar to the above [use the 'enum'!]
+        MaterialButton(
+          color: Colors.blue,
+          child: const Text(
+            "Use Camera",
+            style:
+                TextStyle(color: Colors.white70, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () {
+            getPhoto(context, ImageSourceType.gallery);
+          },
+        ),
+      ],
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Album 571"),
+      ),
+      body: Stack(
+        children: [
+          gridSection,
+          Column(children: [const Spacer(), buttonSection])
+        ],
+      ),
+    );
+  }
+}
+
+// full screen image showing: two classes
+class FullScreenState extends StatefulWidget {
+  final path;
+  FullScreenState(this.path);
+  @override
+  FullScreenImg createState() => FullScreenImg(this.path);
+}
+
+class FullScreenImg extends State<FullScreenState> {
+  var path;
+  var img;
+  var width;
+  var height;
+
+  FullScreenImg(this.path);
+
+  @override
+  void initState() {
+    super.initState();
+    img = Image(image: FileImage(File(path)));
+    // get image width and height
+    img.image.resolve(ImageConfiguration()).addListener(
+      ImageStreamListener(
+        (ImageInfo info, bool _) {
+          width = info.image.width;
+          height = info.image.height;
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Full screen image")),
+        body: Stack(
           children: [
-            Image.asset(
-              'images/xiao.jpeg',
-              width: 600,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection,
-            //menuSection,
+            Image.file(File(path)),
+            Column(
+              children: [
+                const Spacer(),
+                const Text("Image width:"),
+                Text(width.toString()),
+                const Text("Image height"),
+                Text(height.toString())
+              ],
+            )
           ],
-        ),*/
+        ));
+  }
+}
+
+// These two classes are from: https://blog.logrocket.com/building-an-image-picker-in-flutter/
+class ImageFromGalleryEx extends StatefulWidget {
+  final type;
+  ImageFromGalleryEx(this.type);
+
+  @override
+  ImageFromGalleryExState createState() => ImageFromGalleryExState(this.type);
+}
+
+class ImageFromGalleryExState extends State<ImageFromGalleryEx> {
+  var _image;
+  var imagePicker;
+  var type;
+
+  ImageFromGalleryExState(this.type);
+
+  @override
+  void initState() {
+    super.initState();
+    imagePicker = ImagePicker();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(type == ImageSourceType.camera
+              ? "Image from Camera"
+              : "Image from Gallery")),
+      body: Column(
+        children: <Widget>[
+          const SizedBox(
+            height: 52,
+          ),
+          Center(
+            child: GestureDetector(
+              onTap: () async {
+                var source = type == ImageSourceType.camera
+                    ? ImageSource.camera
+                    : ImageSource.gallery;
+                XFile image = await imagePicker.pickImage(
+                    source: source,
+                    imageQuality: 50,
+                    preferredCameraDevice: CameraDevice.front);
+
+                Directory appDocumentsDirectory =
+                    await getApplicationDocumentsDirectory();
+                // correct path to save file for this app
+                String prePath = appDocumentsDirectory.path;
+                var rand = Random();
+                // random int as the photo name
+                String randomInt = rand.nextInt(99999999).toString();
+                String newPath = "$prePath/$randomInt.jpg";
+                File oldImage = File(image.path);
+                // copy the image from cache to a safe place
+                final File newImage = await oldImage.copy(newPath);
+
+                setState(() {
+                  _image = newImage;
+                  // save the path to a global variable
+                  // then the root view can update the grid list
+                  photoList.add(newImage.path);
+                });
+              },
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(color: Colors.red[200]),
+                child: _image != null
+                    ? Image.file(
+                        _image,
+                        width: 200.0,
+                        height: 200.0,
+                        fit: BoxFit.fitHeight,
+                      )
+                    : Container(
+                        decoration: BoxDecoration(color: Colors.red[200]),
+                        width: 200,
+                        height: 200,
+                        child: Icon(
+                          Icons.camera_alt,
+                          color: Colors.grey[800],
+                        ),
+                      ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
